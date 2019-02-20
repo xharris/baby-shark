0722c40b796e50e1e62075de5f3b11e7d4eb3ec1
Xavier Harris <xhh950@gmail.com>
2019-02-20 10:29:39 -0500

    Add files via upload

---
+++ baby_shark.py
@@ -0,0 +1,2 @@
+print('\n'.join([((shark+(","," shark,")[s<5]+(" doo"*6)+"\n")*3)+shark+(""," shark")[s<5]+"!\n" for s, shark in enumerate(["Baby","Mommy","Daddy","Grandma","Grandpa","Let's go hunt","Run away","Safe at last","It's the end"])]))
+    
